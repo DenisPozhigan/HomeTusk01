f7377825238cfd3da22c34b7231440507155c46c
Denis Pozhigan <denis4you@tut.by>
2023-04-15 18:06:31 +0300

    add решение задачи 4

---
+++ Задача4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4
+Console.WriteLine("Введите 3 числа:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int max = a;
+
+            if (b > max)
+            {
+                max = b;
+            }
+
+            if (c > max)
+            {
+                max = c;
+            }
+
+            Console.WriteLine("Наибольшее из введённых чисел " + max);
