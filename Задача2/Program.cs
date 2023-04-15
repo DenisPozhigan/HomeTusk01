aacd71f5743ca1f9d6152ad80791b9716e40d97b
Denis Pozhigan <denis4you@tut.by>
2023-04-15 18:00:17 +0300

    add решение задачи 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Домашняя задача №2
+Console.WriteLine("Введите первое число");
+int a = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse (Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("Число a больше числа b");
+}
+if (a==b)
+{
+    Console.WriteLine("Число b равно числу а");
+}
+else
+{
+    Console.WriteLine("Число b больше числа a");
+}
