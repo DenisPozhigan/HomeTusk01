dfae6023a45d18b8934fe7c5b382447d1c27a0e4
Denis Pozhigan <denis4you@tut.by>
2023-04-15 18:09:47 +0300

    add решение задачи 6

---
+++ Задача6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Домашняя задача 06
+Console.WriteLine("Введите число:");
+           int a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 1)
+            {
+                Console.WriteLine("Число " + a + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + a + " является: ЧЁТНЫМ");
+            }
+
